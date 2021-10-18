19d38eca1cf7edb37afccd4af0654bd795db3954
Szkola <Szkola@S24K01>
2021-10-18 12:22:38 +0200

    Dodaj pliki projektów.

---
+++ WpfApp1/MainWindow.vb
@@ -0,0 +1,15 @@
+﻿Namespace MainWindow
+    {
+    Partial Public Class MainWindow :   Window
+
+    Public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Private void btnStart_Click(Object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Witaj, świecie!");
+        }
+End Namespace
+
